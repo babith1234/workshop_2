95c4ebae62205a2cb588c3bead8f6ef1ec73adb2
babith1234 <you@example.com>
2023-06-27 20:43:22 +0530

    COMMIT

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+const images = ["webdev.jpg", "python.png", "uiux.png"];
+const header = ["webdev","python","uiux"]
+let currentIndex = 0;
+const image = document.getElementById("myImg");
+const text =document.getElementById("id01");
+
+function previousImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  image.src = images[currentIndex];
+  document.getElementById("id01").innerText = header[currentIndex];
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  image.src = images[currentIndex];
+  document.getElementById("id01").innerText = header[currentIndex];
+
+}
